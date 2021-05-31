aa47e0f28413bbf8594ed84a503e3ffb8be9ac3a
Kavi Gupta <kavi@berkeley.edu>
2021-05-31 12:47:46 -0700

    configurable export

--- export.py
+++ export.py
@@ -1,9 +1,15 @@
 import os
+import sys
+
 from qgis.core import QgsProject, QgsLayoutExporter, QgsApplication
+
+
+_, title, path, dpi = sys.argv
+dpi = int(dpi)
 
 QgsApplication.setPrefixPath("/usr", True)
 
-gui_flag = True
+gui_flag = False
 app = QgsApplication([], gui_flag)
 
 app.initQgis()
@@ -13,13 +19,16 @@ project_path = os.path.abspath("statesmap.qgs")
 project_instance = QgsProject.instance()
 project_instance.setFileName(project_path)
 project_instance.read()
+project_instance.setTitle(title)
 
 manager = QgsProject.instance().layoutManager()
 layout = manager.layoutByName("Main")
 
 exporter = QgsLayoutExporter(layout)
+settings = QgsLayoutExporter.ImageExportSettings()
+settings.dpi = dpi
 exporter.exportToImage(
-    project_instance.absolutePath() + "/layout.png",
-    QgsLayoutExporter.ImageExportSettings(),
+    os.path.abspath(path),
+    settings
 )
 app.exitQgis()
