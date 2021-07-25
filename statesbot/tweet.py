c4eb2b474e70718acda3fa859349b3273537aadc
Kavi Gupta <kavi@berkeley.edu>
2021-07-24 20:39:25 -0400

    add retry

--- statesbot/tweet.py
+++ statesbot/tweet.py
@@ -14,7 +14,13 @@ USER_ID = "bot_states"
 
 
 def get_api():
-    my_auth = tweepy.OAuthHandler(my_consumer_key, my_consumer_secret)
+    my_auth = tweepy.OAuthHandler(
+        my_consumer_key,
+        my_consumer_secret,
+        retry_count=10,
+        retry_delay=5,
+        retry_errors=set([503]),
+    )
     my_auth.set_access_token(my_access_token, my_access_token_secret)
     return tweepy.API(my_auth)
 
